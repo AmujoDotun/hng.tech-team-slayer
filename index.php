e32f250414b3f598eccfefaf115987edcd6f01c2
fakorede <abiolafakorede@gmail.com>
2019-04-06 16:30:50 +0100

    image upload system from abiola fakorede

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Image Uploader</title>
+</head>
+<body>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="image">
+        <button type="submit" name="submit">Upload Image</button>
+    </form>
+</body>
+</html>
